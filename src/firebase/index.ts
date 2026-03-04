f546b352fde7c6aa760f798d8f1f2d8b1d109e4f
Sure Pay <surepaytz@gmail.com>
2026-03-04 11:23:28 +0000

    Try fixing this error: `Build Error: Export useMemoFirebase doesn't exis

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,6 +6,7 @@ export { FirebaseClientProvider } from './client-provider';
 export { useCollection } from './firestore/use-collection';
 export { useDoc } from './firestore/use-doc';
 export { useUser } from './auth/use-user';
+export { useMemoFirebase } from './use-memo-firebase';
 
 export function initializeFirebase() {
   const firebaseApp = getFirebaseApp();
